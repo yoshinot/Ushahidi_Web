2821189fca32927ed3ec726f67286db175b573d1
Nachi Ueno <nati.ueno@gmail.com>
2011-03-16 16:46:27 +0900

    Skip img tag which invoke twitter crawler #100wq

--- themes/default/views/footer.php
+++ themes/default/views/footer.php
@@ -54,8 +54,9 @@
 	<?php echo $google_analytics; ?>
 	
 	<!-- Task Scheduler -->
+        <!--
 	<img src="<?php echo url::base(); ?>media/img/spacer.gif" alt="" height="1" width="1" border="0" onload="runScheduler(this)" />
- 
+        -->
 	<?php
 	// Action::main_footer - Add items before the </body> tag
 	Event::run('ushahidi_action.main_footer');
