23b994c6d35bd11c56e3a84d98e018c563085755
TAKANO Mitsuhiro <mitsuhiro.takano@gree.co.jp>
2011-03-17 15:21:37 +0900

    already exists tracking code at *footer* ...

--- themes/default/views/header.php
+++ themes/default/views/header.php
@@ -8,21 +8,6 @@
 	// Action::header_scripts - Additional Inline Scripts from Plugins
 	Event::run('ushahidi_action.header_scripts');
 	?>
-<!-- Start of Google Analytics -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-177918-4']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<!-- End of Google Analytics -->
 </head>
 
 <body id="page">
