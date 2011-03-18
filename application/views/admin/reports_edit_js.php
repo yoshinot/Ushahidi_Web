7bc23e190924109d64d5212d1132993ce9b07903
TAKANO Mitsuhiro <mitsuhiro.takano@gree.co.jp>
2011-03-18 17:43:36 +0900

    set default {lat,long}itude, location are Tokyo

--- application/views/admin/reports_edit_js.php
+++ application/views/admin/reports_edit_js.php
@@ -231,6 +231,9 @@
 				$("#latitude").attr("value", lonlat2.lat);
 				$("#longitude").attr("value", lonlat2.lon);
 			});
+			$("#latitude").attr("value", 35.689487);
+			$("#longitude").attr("value", 139.691706);
+			$("#location_name").attr("value", "東京都");
 			
 			// GeoCode
 			$('.btn_find').live('click', function () {
