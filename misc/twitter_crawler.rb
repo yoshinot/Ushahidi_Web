8ea335e2af8338cc33f98010528403d892171ac7
TAKANO Mitsuhiro <mitsuhiro.takano@gree.co.jp>
2011-03-18 20:57:34 +0900

    add ignore keywords to twitter crowler

--- misc/twitter_crawler.rb
+++ misc/twitter_crawler.rb
@@ -70,8 +70,8 @@ def put_to_db(tweet)
              1
            when /live at/, /ustre\.?am/, /#nhk/, /nico\.ms/, /lv\d+/, /nicovideo/
              2
-           when /GIZMODO/i, /GIGAZINE/i
-             3
+           when /GIZMODO/i, /GIGAZINE/i, /NEWS4VIP/i, /APPBANK/i, /MAINICHI/i, /HEADLINES\.YAHOO/i
+            3
            when /拡散希望/, /コピペ/, /followme/
              4
            else; 0
