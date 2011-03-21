e9a5fd48b867854e6be2f077d633d20c6db5c7e9
Shinsuke ARAKAWA <potot.super@gmail.com>
2011-03-21 12:48:06 +0900

    Add TasukeaiJapan pages #129

---
+++ plugins/tasukeaijapan/views/tasukeaijapan/footer.php
@@ -0,0 +1,11 @@
+			</div>
+    <div id="footer_link"><a href="<?php echo url::base(); ?>" target="_blank"><?php echo $site_name; ?></a></div>
+		</div>
+		<!-- / main body -->
+	</div>
+	<!-- / wrapper -->
+ 
+	<?php echo $ushahidi_stats; ?>
+	<?php echo $google_analytics; ?>
+</body>
+</html>
