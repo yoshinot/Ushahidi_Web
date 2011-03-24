6891e6fa5b039e366218cffdae2b929b7df45279
hagino3000 <hagino.3000@gmail.com>
2011-03-24 13:36:50 +0900

    リンクの場所を変更

--- themes/default/views/main.php
+++ themes/default/views/main.php
@@ -167,16 +167,6 @@
 						<h3>レポートを地図から探す</h3>
                         <p>◎をクリックすると、そのエリアで投稿されたレポートがご覧になれます。</p>
 					</div>
-                                        <div style="float:right;width:100px;">
-					<?php
-					// Action::main_filters - Add items to the main_filters
-					//Event::run('ushahidi_action.map_main_filters');
-					$menu = "";
-                                        $menu .= "<h1><a target=\"bigmap\" href=\"".url::site()."bigmap\" ";
-                                        $menu .= ">全画面表示</a></h1>";
-                                        echo $menu;
-                                        ?>
-                                        </div>
                                 </div>
 				<!-- / filters -->
 				
@@ -185,6 +175,17 @@
 				echo $div_map;
 				echo $div_timeline;
 				?>
+                                <div style="float:right;width:100px;">
+                                        <?php
+                                        // Action::main_filters - Add items to the main_filters
+                                        //Event::run('ushahidi_action.map_main_filters');
+                                        $menu = "";
+                                        $menu .= "<h2><a target=\"bigmap\" href=\"".url::site()."bigmap\" ";
+                                        $menu .= ">全画面表示</a></h2>";
+                                        echo $menu;
+                                        ?>
+                                </div>
+
 			</div>
 		</div>
 		<!-- / content column -->
