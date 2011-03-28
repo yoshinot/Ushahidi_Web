2b70af1cc92ef8c42328e5828a417efc4a71b56b
ggslyman <ggslyman@gmail.com>
2011-03-28 18:02:45 +0900

    #283 footer update

--- themes/default/views/footer.php
+++ themes/default/views/footer.php
@@ -23,26 +23,27 @@
 			<div class="footermenu">
 				<ul class="clearingfix">
 					<li><a class="item1" href="<?php echo url::site(); ?>"><?php echo Kohana::lang('ui_main.home'); ?></a></li>
-					<li><a href="<?php echo url::site()."reports/submit"; ?>">レポートを投稿</a></li>
-					<li><a href="<?php echo url::site()."alerts"; ?>"><?php echo Kohana::lang('ui_main.alerts'); ?></a></li>
+					<?php /*<li><a href="<?php echo url::site()."reports/submit"; ?>">レポートを投稿</a></li>*/?>
+					<?php /*<li><a href="<?php echo url::site()."alerts"; ?>"><?php echo Kohana::lang('ui_main.alerts'); ?></a></li>*/?>
 					<li><a href="<?php echo url::site()."page/index/9"; ?>">サイト運営団体について<!--<?php echo Kohana::lang('ui_main.help'); ?>--></a></li>
 					<li><a href="<?php echo url::site()."contact"; ?>"><?php echo Kohana::lang('ui_main.contact'); ?></a></li>
+					<li><a href="https://spreadsheets.google.com/viewform?formkey=dGlnajlENUtFOFZnWlN2XzlqbklickE6MQ"><?php echo Kohana::lang('ui_main.feedback'); ?></a></li>
 					<?php
 					// Action::nav_main_bottom - Add items to the bottom links
 					Event::run('ushahidi_action.nav_main_bottom');
 					?>
 				</ul>
 				<?php if($site_copyright_statement != '') { ?>
-      		<p><?php echo $site_copyright_statement; ?></p>
+      		<p class="copyright"><?php echo $site_copyright_statement; ?></p>
       	<?php } ?>
 			</div>
 			<!-- / footer menu -->
 
-      
+<?php /*  
 			<h2 class="feedback_title" style="clear:both">
 				<a href="https://spreadsheets.google.com/viewform?formkey=dGlnajlENUtFOFZnWlN2XzlqbklickE6MQ"><?php echo Kohana::lang('ui_main.feedback'); ?></a>
 			</h2>
-
+*/?>
  
 		</div>
 		<!-- / footer content -->
